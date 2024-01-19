efb735cc8ec194e33712a4d717a21eeabb63451a
yabluko <yurarobak7@gmail.com>
2024-01-19 17:59:19 +0200

    Do something wrong

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,30 +4,30 @@ import Badge from '../Badge/Badge'
 import classNames from 'classnames';
 import remove from '../../assets/img/remove.svg'
 
-function List({items , onClick, isRemovable, onRemove, onClickItem, activeItem}){
-
+function List({ items, onClick, isRemovable, onRemove, onClickItem, activeItem }) {
+    //Wrong comment
     const removeElement = (item) => {
-        fetch('http://localhost:3001/lists/' + item.id  ,{
+        fetch('http://localhost:3001/lists/' + item.id, {
             method: 'DELETE'
         }).then(() => {
             onRemove(item.id)
         })
     }
-    return(
+    return (
 
-        <ul onClick={onClick}  className="list"> 
-            { 
-            items.map((item , index) => (
-                <li onClick={onClickItem ? () => onClickItem(item) : null} key={index} className={classNames(item.className, {active : item.active ? item.active : activeItem && activeItem.id === item.id})}>
+        <ul onClick={onClick} className="list">
+            {
+                items.map((item, index) => (
+                    <li onClick={onClickItem ? () => onClickItem(item) : null} key={index} className={classNames(item.className, { active: item.active ? item.active : activeItem && activeItem.id === item.id })}>
                         <div>
-                        <i>
-                            {item.icon ? item.icon : <Badge color={item.color.name} /> }
-                        </i>
-                        <span>{item.name} {item.tasks && `(${item.tasks.length})`}</span>
+                            <i>
+                                {item.icon ? item.icon : <Badge color={item.color.name} />}
+                            </i>
+                            <span>{item.name} {item.tasks && `(${item.tasks.length})`}</span>
                         </div>
-                        {isRemovable && <img onClick={() =>  removeElement(item)} className="list__remove-icon" src={remove} alt="x" />}
+                        {isRemovable && <img onClick={() => removeElement(item)} className="list__remove-icon" src={remove} alt="x" />}
                     </li>
-            ))
+                ))
             }
         </ul>
     )
